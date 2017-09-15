84046ac951126d971b36a1323f5cc5cc5e582330
skkwon <skkwon.partner@iloen.com>
2017-09-15 19:56:44 +0900

    * debounce 와 distinctUntilChange 를 해줌.

--- RxTable101/ViewController.swift
+++ RxTable101/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    let allCities = ["Seoul","Pusan","New York", "London", "Oslo", "Moscow", "Berlin", "Praga"]
+    let allCities = ["Seoul","Seo","Pakan","Pusan","New Hampshire","New York", "London", "Oslo","OMG","Madrid", "Moscow", "Berlin", "Praga"]
     
     var favCities :[String] = [];
     
@@ -33,13 +33,14 @@ class ViewController: UIViewController {
     
     func rxSetup(){
         
-        searchBar.rx.text.orEmpty
-            .subscribe(onNext: { (query) in
+        searchBar.rx.text
+            .orEmpty    //옵셔널 벗기기
+            .debounce(0.6, scheduler: MainScheduler.instance) //3초 동안 여유를 준다!!
+            .distinctUntilChanged() //동일한 값인 경우 이전 request 를 cancel 해줌.
+            .subscribe(onNext: {[unowned self] (query) in
                 
                 self.favCities = self.allCities.filter{ $0.hasPrefix(query) }
-                
-                //도시명의 앞부분 철자가 일치하면 favCities 로 넣는다.
-                
+               
                 self.tableView.reloadData()
             }
         )
@@ -47,7 +48,26 @@ class ViewController: UIViewController {
     }
 }
 
+/*설명
+ 
+ * orEmpty     
+ 
+ 옵셔널 벗기기
 
+ 
+ * debounce
+ 
+ 찰나의 순간에도 마음이 변할 수 있다.
+ 방금 타이핑했던 값을 재빠르게 변경할때, 최초 입력값은 무시해준다.!!
+ 
+
+ * distinctUntilChanged
+ 
+ 동일한 값이 나오면 매번 refresh 하는데
+ 이를 사용하면 중복값일 때 refresh 하지 않는다. 
+ 즉 중복 request 하지 않는다고 생각하면 됨.
+ 
+*/
 
 // MARK: - UITableView 데이터소스만 확장해주면 됩니다.
 
